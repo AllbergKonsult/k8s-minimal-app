bf1e725f14b86658d5ab5ec51bf5befa2b5e3b86
John Allberg <john@ayoy.se>
2024-03-24 18:05:48 +0100

    Add support for delaying readiness probe.

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+var start = time.Now()
+var readinessDelayFlag = flag.Int("delay-ready", 0, "Seconds after start to report ready=false")
 
 var httpRequestsTotal = prometheus.NewCounter(
 	prometheus.CounterOpts{
@@ -20,12 +25,33 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	httpRequestsTotal.Inc()
 	msg := "Received a request"
 	fmt.Fprint(w, msg)
-	fmt.Println(msg)
+	log.Println(msg)
+}
+
+func readiness(w http.ResponseWriter, r *http.Request) {
+	httpRequestsTotal.Inc()
+	duration := time.Since(start)
+	msg := fmt.Sprintf("Received a readiness request after %s", duration)
+	var readinessAfter = time.Second * time.Duration(*readinessDelayFlag)
+	log.Printf("readinessAfter: %s", readinessAfter)
+	if duration < readinessAfter {
+		w.WriteHeader(400)
+		msg = msg + " - unready"
+	} else {
+		w.WriteHeader(200)
+		msg = msg + " - ready"
+	}
+	fmt.Fprint(w, msg+"\n")
+	log.Println(msg)
 }
 
 func main() {
 	port := "8080"
+	start = time.Now()
+	flag.Parse()
+	log.Printf("Delayflag: %d", *readinessDelayFlag)
 	prometheus.MustRegister(httpRequestsTotal)
+	http.HandleFunc("/readyz", readiness)
 	http.HandleFunc("/", handler)
 	http.Handle("/metrics", promhttp.Handler())
 	log.Printf("Server started on port %v", port)
